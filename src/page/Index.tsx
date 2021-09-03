886934066d7811ecd0207837cab2e9c093cfa2e0
ANNG <annyhanyuu@gmail.com>
2021-09-03 17:46:54 +0800

    1

--- src/page/Index.tsx
+++ src/page/Index.tsx
@@ -3,17 +3,20 @@ import { Row, Col, Tabs, Table, Button, Tag, Tooltip } from "antd";
 import "@/static/css/Index.scss";
 import axios from "axios";
 import moment from "moment";
+import { CaretRightOutlined } from '@ant-design/icons';
 const { TabPane } = Tabs;
 export default class Index extends React.Component<any, any>{
 
     public state: {
         systemTableLoading: boolean;
         dataSource: Array<any>;
+        testingTimeButtonLoading: any
     }
 
     constructor(props: any) {
         super(props);
         this.state = {
+            testingTimeButtonLoading: {},
             systemTableLoading: true,
             dataSource: []
         }
@@ -29,14 +32,17 @@ export default class Index extends React.Component<any, any>{
             title: '最后同步时间',
             dataIndex: 'testing_time',
             key: 'testing_time',
-            render: (value: number, row: any) => {
-                return <>{moment.unix(value).format('YYYY-MM-DD HH:mm')}&nbsp;<Button size="small" type="primary" onClick={() => this.sync(row.id)}>同步</Button></>
-            }
+            width: 200,
+            align: 'center' as 'center',
+            render: (value: number, row: any) => <><div style={{ textAlign: 'center' }}>{moment.unix(value).format('YYYY-MM-DD HH:mm')}&nbsp;<Button icon={<CaretRightOutlined />} size="small" type="primary" loading={this.state.testingTimeButtonLoading[row.id] === undefined ? false : this.state.testingTimeButtonLoading[row.id]} onClick={() => this.sync(row.id)}>同步</Button></div></>
+
         },
         {
             title: '原因',
             dataIndex: 'reason_msg',
             key: 'reason_msg',
+            width: 100,
+            align: 'center' as 'center',
             render: (value: string, row: any) => {
                 if (row.reason_status === 1) {
                     return <Tag color="green">成功</Tag>
@@ -48,6 +54,10 @@ export default class Index extends React.Component<any, any>{
     ];
 
     componentDidMount = async () => {
+        this.list();
+    }
+
+    list = async () => {
         const { data } = await axios.get('http://127.0.0.1:83/bt/system/ipList');
         this.setState({
             dataSource: data.data,
@@ -55,10 +65,20 @@ export default class Index extends React.Component<any, any>{
         });
     }
 
-
     sync = async (id: number) => {
-        const data = await axios.post('http://127.0.0.1:83/bt/system/sync', { id: id });
+        let loading: any = {};
+        loading[id] = true;
+        this.setState({
+            testingTimeButtonLoading: Object.assign(this.state.testingTimeButtonLoading, loading)
+        })
+        const data = await axios.post('http://127.0.0.1:83/bt/system/sync?time', { id: id });
         console.log(data)
+        loading[id] = false;
+        this.setState({
+            testingTimeButtonLoading: Object.assign(this.state.testingTimeButtonLoading, loading)
+        })
+
+        this.list();
     }
 
     render = () => {
@@ -86,7 +106,7 @@ export default class Index extends React.Component<any, any>{
             <div className="list">
                 <Tabs defaultActiveKey="1" >
                     <TabPane tab="服务器" key="1">
-                        <Table dataSource={this.state.dataSource} columns={this.columns} loading={this.state.systemTableLoading} />;
+                        <Table dataSource={this.state.dataSource} columns={this.columns} loading={this.state.systemTableLoading} bordered />;
                     </TabPane>
                     <TabPane tab="网站" key="2">
                         {/* <Table dataSource={this.dataSource} columns={this.columns} />; */}
